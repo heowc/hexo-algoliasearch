c4609c0f6e58dcd60a2d2ba60782348783d1b011
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2017-11-23 22:37:28 -0800

    WIP

--- index.js
+++ index.js
@@ -1,113 +1,90 @@
+/* eslint-disable camelcase */
 'use strict';
-
 var path = require('path');
-var chalk = require('chalk');
-var _ = require('lodash');
-var green = chalk.green;
 
 var initPosts = require('./lib/initPosts');
-var processPost = require('./lib/processPost');
 var Algolia = require('./lib/algolia');
 var AlgoliaLocal = require('./lib/algoliaLocal');
-var addAlgoliaObjectId = require('./lib/hooks/addAlgoliaObjectId');
+var initPost = require('./lib/hooks/initPost');
+var synchronizePosts = require('./lib/synchronizePosts');
 
-var createdFilenames = [];
-var editedFilenames = [];
-var deletedFilenames = [];
 var createdPosts = [];
-var editedPosts = [];
+var updatedPosts = [];
 var deletedPosts = [];
+// use this variable to not run portion of code if server is started to improve performance
+var isServerStarted = false;
+var algoliaLocal = AlgoliaLocal.getInstance();
 
-// Add a unique identifier in the front-matter of the post created
-hexo.on('new', addAlgoliaObjectId);
+// add a unique identifier in the front-matter of the post created
+hexo.on('new', initPost);
 
-hexo.extend.processor.register('_posts/', function(file) {
-  if (file.type === 'create') {
-    createdFilenames.push(file.source);
-  }
-  else if (file.type === 'update') {
-    editedFilenames.push(file.source);
-  }
-  else if (file.type === 'delete') {
-    /* eslint-disable camelcase */
-    deletedFilenames.push({full_source: file.source});
-    /* eslint-enable camelcase */
+// detect changes (creation, update, deletion) on posts
+hexo.extend.processor.register(/_posts\/.*?\.md$/, function(file) {
+  if (file.type !== 'skip') {
+    algoliaLocal.addPost(file.source, file.type, false);
   }
 });
 
 hexo.on('generateBefore', function() {
-  // register filter to collect data of each posts
-  hexo.extend.filter.register('after_post_render', function(post) {
-    if (post.published) {
-      // init post on the fly - we add a unique identifier for Algolia
-      if (!post.algolia_object_id) {
-        addAlgoliaObjectId(post);
+  // save modifications into a file to sync this file later after deployment
+  algoliaLocal.save();
+  // register only one time this filter in case
+  // the server is started to prevent multiple registrations
+  if (!isServerStarted) {
+    // register filter to collect data of each posts and synchronize them after deployment
+    hexo.extend.filter.register('after_post_render', function(post) {
+      // do not execute filter if server is running to improve performance
+      if (isServerStarted) {
+        return;
+      }
+      if (post.published) {
+        // init post on the fly
+        if (!post.algolia_object_id) {
+          initPost(post);
+        }
+
+        if (algoliaLocal.getCreatedPosts().indexOf(post.full_source) > -1) {
+          createdPosts.push(post);
+        }
+        else if (algoliaLocal.getUpdatedPosts().indexOf(post.full_source) > -1) {
+          updatedPosts.push(post);
+        }
       }
 
-      if (createdFilenames.indexOf(post.full_source) > -1) {
-        createdPosts.push(post);
-      }
-      else if (editedFilenames.indexOf(post.full_source) > -1) {
-        editedPosts.push(post);
-      }
-    }
-    return post;
-  });
+      return post;
+    });
+  }
 });
 
-hexo.on('deployAfter', function() {
-  var config = this.config.algolia;
-  var algolia = new Algolia(config);
-  var algoliaLocal = new AlgoliaLocal(config);
+// synchronize posts after deployment
+hexo.on('deployAfter', synchronizePosts);
 
-  // Due to a bug from Hexo, filter created posts because when a post is added,
-  // all posts are considered as `create`
-  createdPosts = _.pullAllBy(createdPosts, algoliaLocal.posts, 'algolia_object_id');
-
-  // process post before indexation
-  var postsToIndex = createdPosts.concat(editedPosts).map(function(post) {
-    return processPost(post, config);
-  });
-
-  // get `algolia_object_id` of deleted posts
-  deletedPosts = _.intersectionBy(algoliaLocal.posts, deletedFilenames, 'full_source');
-  deletedPosts = deletedPosts.map(function(post) {
-    /* eslint-disable camelcase */
-    return post.algolia_object_id;
-    /* eslint-enable camelcase */
-  });
-
-  // index new posts & edited posts
-  algolia.saveObjects(postsToIndex, function(err) {
-    if (err) {
-      throw err;
-    }
-    // add new posts to the local index
-    algoliaLocal.addObjects(createdPosts);
-    // remove deleted posts on Algolia
-    algolia.deleteObjects(deletedPosts, function(err) {
-      if (err) {
-        throw err;
-      }
-      // remove deleted posts of the local index
-      algoliaLocal.deleteObjects(deletedFilenames);
-      console.log(green('INFO  ') + 'Indexation done. Index is up-to-date.');
-    });
-  });
+// define `isServerStarted` to `true` to prevent execution of
+// portion of code to improve performance during writing posts
+hexo.on('exit', function() {
+  isServerStarted = true;
 });
 
 // register `hexo algolia` command
 hexo.extend.console.register('algolia', 'Index your posts on Algolia', {
   options: [{
-    name: '-n, --no-clear', desc: 'Does not clear the existing index'
-  }, {
     name: '-i, --init', desc: 'Add a unique identifier on each post'
+  }, {
+    name: '-s, --sync', desc: 'Synchronize posts'
+  }, {
+    name: '-c, --clear', desc: 'Clear index on Algolia'
   }]
-}, function(args) {
-  var hexo = this;
-  var postsDir = path.resolve(process.cwd(), hexo.config.source_dir, '_posts');
+}, function(args, callback) {
+  var postsDir = path.resolve(process.cwd(), this.config.source_dir, '_posts');
 
   if (args.i || args.init) {
     initPosts(postsDir);
   }
+
+  if (args.c || args.clear) {
+    var algolia = new Algolia(this.config.algolia);
+    algoliaLocal.clear();
+    algolia.clearIndex();
+  }
 });
+/* eslint-enable camelcase */
